a0f8ea7779e550bd15d5e8c678345406f9702c39
dsta50 <94760363+dsta50@users.noreply.github.com>
2023-05-07 20:29:00 +0900

    Remove dryrun logic and fix typo

--- aws-security-group_rule.sh
+++ aws-security-group_rule.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # Usage: 
-#   security_group_rule.sh <add|del> <in|out> <rule_list(csv)> [dryrun]
+#   security_group_rule.sh <add|del> <in|out> <rule_list(csv)>
 #     add/del: Specify "add" to add rules or "del" to delete rules.
 #     in/out: Inboud or Outbound
 #     rule_list.csv: The path to the CSV file containing the list of rules to add or delete.
-#     dryrun: Not required.
 # Description:
 #  This script can add or delete rules for existing security groups using the AWS CLI.
 #  The CSV file should include the security group ID for the target security group.
@@ -15,24 +14,15 @@ LOG_DIR=$(cd $(dirname ${0}); pwd)/log
 LOGFILE=${LOG_DIR}/aws-sg_rule_`date '+%Y%m%d-%H%M%S'`.log
 
 AWSCLI_CMD='aws ec2'
-USAGE="$0 <add|del|addshow|delshow> <in|out> <listfile(csv)> [dryrun]"
+USAGE="$0 <add|del|addshow|delshow> <in|out> <listfile(csv)>"
 
 ACTION=$1
 DIRECTION=$2
 RULE_LIST=$3
-DRYRUN_FLG=$4
 
 # Argument Count Check
-if [ $# -ne 3 -a $# -ne 4 ]; then
+if [ $# -ne 3 ]; then
     echo -e "Error: Invalid number of arguments"
-    echo -e "Usage: ${USAGE}"
-    exit 1
-fi
-
-if [[ "${DRYRUN_FLG}" == "dryrun" ]]; then
-    CMD_SUFFIX='--dry-run'
-elif [[ -n "${DRYRUN_FLG}" ]]; then
-    echo -e "Error: Invalid argument: ${DRYRUN_FLG}"
     echo -e "Usage: ${USAGE}"
     exit 1
 fi
@@ -102,12 +92,12 @@ function func_create_cmd() {
     fi
     
     # Assembling commands
-    CMD="${AWSCLI_CMD} ${SUBCOMD_TMP} ${CMD_SUFFIX}"
+    CMD="${AWSCLI_CMD} ${SUBCOMD_TMP}"
 }
 
 # Main
 # Read and process rules from list file
-while IFS=',' read -r SECURITY_GROUP_ID PROTOCOL FROM_PORT TO_PORT CIDR DESCRIPTION
+while IFS=',' read -r SECURITY_GROUP_ID PROTOCOL FROM_PORT TO_PORT CIDER_SGID DESCRIPTION
 do
     case "${ACTION}" in
         "add" )
